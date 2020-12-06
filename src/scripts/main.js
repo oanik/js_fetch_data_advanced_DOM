48446a23de9777aa5925a5f6b0e32dd8efcdf4a2
Olga Rubalska <olga.anikeieva@gmail.com>
2020-12-06 18:24:53 +0200

    Promises Advanced

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,56 @@
 'use strict';
 
-// write code here
+// eslint-disable-next-line max-len
+const basicURL = 'https://mate-academy.github.io/phone-catalogue-static/api/phones';
+
+const request = (url) => {
+  return fetch(`${url}.json`)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`${response.status}`);
+      }
+
+      return response.json();
+    });
+};
+
+const getIDsArray = (array) => {
+  const arrayOfIds = [];
+
+  array.forEach(element => {
+    arrayOfIds.push(element.id);
+  });
+
+  return arrayOfIds;
+};
+
+const getFirstReceivedDetails = (iDs, baseUrl) => {
+  Promise.race(iDs.map(id => fetch(`${baseUrl}/${id}.json`)))
+    .then(result => {
+      return result.json();
+    })
+    .then(result => console.log(result));
+};
+
+const getAllSuccessfulDetails = (iDs, baseUrl) => {
+  Promise.allSettled(iDs.map(id => fetch(`${baseUrl}/${id}.json`)
+    .then(result => {
+      return result.json();
+    })))
+    .then(results => {
+      results.forEach((result, num) => {
+        console.log(`${iDs[num]} with details:`, result.value);
+      });
+    });
+};
+
+const getPhones = () => request(basicURL);
+
+getPhones()
+  .then(result => {
+    const arrayOfIds = getIDsArray(result);
+
+    getFirstReceivedDetails(arrayOfIds, basicURL);
+    getAllSuccessfulDetails(arrayOfIds, basicURL);
+  })
+  .catch(error => console.log(error, 'Error has occured!'));
